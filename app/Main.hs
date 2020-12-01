f70e748626e569d355138f9b89f3a39a2f2ef1de
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2020-12-01 17:42:24 +0000

    main: Day01 Part 2

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Day01
 
 main :: IO ()
-main = putStr =<< (unlines <$> map show <$> day01Pt1Solution)
+main = putStr =<< (unlines <$> map show <$> day01Pt2Solution)
--- src/Day01.hs
+++ src/Day01.hs
@@ -1,19 +1,39 @@
 module Day01
     (
-    day01Pt1Solution
+    day01Pt1Solution,
+    day01Pt2Solution
     ) where
 
 day01Pt1Solution :: IO [Int]
 day01Pt1Solution = map multiplyPair <$> (findMatchingPair 2020) <$> readFileofInts
 
+day01Pt2Solution :: IO [Int]
+day01Pt2Solution = map multiplyTriple <$> (findMatchingTriple 2020) <$> readFileofInts
+
 multiplyPair :: (Int, Int) -> Int
 multiplyPair (a, b) = a*b
+
+multiplyTriple :: (Int, Int, Int) -> Int
+multiplyTriple (a, b, c) = a * b * c
 
 findMatchingPair:: (Num a, Ord a, Eq a) => a -> [a] -> [(a, a)]
 findMatchingPair target list = filter (\l -> (fst l + snd l) == target) $ crossList list
 
+findMatchingTriple:: (Num a, Ord a, Eq a) => a -> [a] -> [(a, a, a)]
+findMatchingTriple target list = filter (\l -> (fst3 l + snd3 l + thd3 l) == target) $ crossListTriple list
+
+fst3::(a,b,c) -> a
+fst3 (a, b, c) = a
+snd3::(a,b,c) -> b
+snd3 (a, b, c) = b
+thd3::(a,b,c) -> c
+thd3 (a, b, c) = c
+
 crossList:: (Ord a) => [a] -> [(a, a)]
 crossList list = [(x,y) | x <-list, y<-list, x <= y]
+
+crossListTriple:: (Ord a) => [a] -> [(a, a, a)]
+crossListTriple list = [(x,y,z) | x <- list, y <- list, z <- list, x <= y, y <= z]
 
 readFileofInts :: IO [Int]
 readFileofInts = map read <$> lines <$> readFile "resource/day01"
