8d0f32a451eb8e54b5532063331be78799e30705
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2020-12-04 13:10:40 +0000

    main: Day 04 2020 pt 2 wip

--- src/Day04_2020.hs
+++ src/Day04_2020.hs
@@ -2,8 +2,47 @@ module Day04_2020
     (
     ) where
 
-    import Data.List (sort)
-    import Data.String.Utils (split, splitWs, startswith)
+    import Data.List (sort, find)
+    import Data.String.Utils (split, splitWs, startswith, endswith)
+
+    data Metric = CM | IN deriving Show
+    data Height = Height Int Metric deriving Show
+    data EyeColour = AMB | BLU | BRN | GRY | GRN | HZL | OTH deriving Show
+    data Record = Record Int Int Int Height String String String (Maybe Int) deriving Show
+
+    --constructRecord :: [String] -> Maybe Record
+    readIntTag:: String -> [String] -> Maybe Int
+    readIntTag tagName input = read . drop 4 <$> find (startswith tagName) input
+
+    readBirthYear = readIntTag "byr:"
+    readIssueYear = readIntTag "iyr:"
+    readExpirationYear = readIntTag "eyr:"
+
+    readHeight :: [String] -> Maybe Height
+    readHeight input = (parseHeight . drop 4) =<< find (startswith "hgt:") input
+
+    parseHeight :: String -> Maybe Height
+    parseHeight = (\c -> if (endswith "cm" c || endswith "in" c) then Just (Height (read $ take ((length c) - 2) c) (toMetric $ take 2 $ drop ((length c) - 2) c)) else Nothing)
+
+    toMetric :: String -> Metric
+    toMetric "cm" = CM
+    toMetric "in" = IN
+
+    readEyeColour :: [String] -> Maybe EyeColour
+    readEyeColour input = (toEyeColour . drop 4) =<< find (startswith "ecl:") input
+
+    toEyeColour :: String -> Maybe EyeColour
+    toEyeColour "amb" = Just AMB
+    toEyeColour "blu" = Just BLU
+    toEyeColour "brn" = Just BRN
+    toEyeColour "gry" = Just GRY
+    toEyeColour "grn" = Just GRN
+    toEyeColour "hzl" = Just HZL
+    toEyeColour "oth" = Just OTH
+    toEyeColour "amb" = Nothing
+
+    readPassportID :: [String] -> Maybe Int
+    readPassportID input = (_ . drop 4) =<< find (startswith "ecl:") input
 
     runDocuments = map mergeTokeniseSort . groupBetweenBlankLines <$> readDocuments
     countValidDocuments = length . filter validate <$> runDocuments
