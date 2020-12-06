7417939338a68a772865ea075dc72ec3bc68a464
D <daniel.manning@digital.hmrc.gov.uk>
2020-12-06 11:18:58 +0000

    Day 06 2020 pt2

--- src/Day06_2020.hs
+++ src/Day06_2020.hs
@@ -2,7 +2,9 @@ module Day06_2020
     (
     ) where
 
-    import Data.List (nub)
+    import Data.List (nub, foldl1')
+    import Data.Set (Set)
+    import qualified Data.Set as Set
 
     groupBetweenBlankLines :: [String] -> [[String]]
     groupBetweenBlankLines input = groupBetweenBlankLines'' input []
@@ -10,9 +12,14 @@ module Day06_2020
     groupBetweenBlankLines'' :: [String] -> [String] -> [[String]]
     groupBetweenBlankLines'' [] n = [n]
     groupBetweenBlankLines'' (x:xs) n | length x == 0 = n : groupBetweenBlankLines'' xs []
-                                    | otherwise = groupBetweenBlankLines'' xs (x : n)
+                                      | otherwise = groupBetweenBlankLines'' xs (x : n)
 
-    runAnswers = sum <$> (map (length . nub . foldr (++) "") . groupBetweenBlankLines) <$> readAnswers
+    day06pt1 = sum <$> (map (length . nub . foldr (++) "") . groupBetweenBlankLines) <$> readAnswers
+    day06pt2 = sum <$> (map (length . elementsInAllLists) . groupBetweenBlankLines) <$> readAnswers
+
+
+    elementsInAllLists :: (Ord a) => [[a]] -> [a]
+    elementsInAllLists list = Set.toList $ foldl1' (\a b -> a `Set.intersection` b) $ map Set.fromList list
 
     readAnswers :: IO [String]
     readAnswers = lines <$> readFile "resource/2020/day06"
