f806e39fcc3fdf918f09396dae421a5ee79719be
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2020-12-08 20:10:31 +0000

    main: Day 08 pt 2

--- src/Day08_2020.hs
+++ src/Day08_2020.hs
@@ -41,6 +41,7 @@ module Day08_2020
     runLine :: [Instruction] -> ([Int], ProgrammeState) -> Maybe ([Int], ProgrammeState)
     runLine [] _ = Nothing
     runLine instructions (visitedLines, (ProgrammeState l (Accumulator v))) | l `elem` visitedLines = Nothing
+                                                                            | l >= (length instructions) = Nothing
                                                                             | otherwise = Just $ ( l: visitedLines, evalInstruction l v (instructions !! l))
 
     iterateWhileRunning :: (([Int], a) -> Maybe ([Int], a)) -> ([Int], a) -> [([Int], a)]
@@ -53,6 +54,45 @@ module Day08_2020
     runInstructions instructions = iterateWhileRunning (runLine instructions) ([], (ProgrammeState 0 (Accumulator 0)))
 
     day08Pt1 = last . runInstructions <$> map (fromRight' . parse parseInstruction "") <$> readInstructions
+
+-----------------------
+    runLine'' :: [Instruction] -> Maybe Int -> ([(Int, Instruction)], ProgrammeState) -> Maybe ([(Int, Instruction)], ProgrammeState)
+    runLine'' [] _ _ = Nothing
+    runLine'' instructions Nothing (visitedLines, (ProgrammeState l (Accumulator v))) | l `elem` (map fst visitedLines) = Nothing
+                                                                                      | l >= (length instructions) = Nothing
+                                                                                      | otherwise = Just $ ( (l, (instructions !! l)): visitedLines, evalInstruction l v (instructions !! l))
+    runLine'' instructions (Just lR) (visitedLines, (ProgrammeState l (Accumulator v))) | l `elem` (map fst visitedLines) = Nothing
+                                                                                              | l >= (length instructions) = Nothing
+                                                                                              | l == lR = Just $ ( (l, (swapInstruction (instructions !! l))): visitedLines, evalInstruction l v (swapInstruction (instructions !! l)))
+                                                                                              | otherwise = Just $ ( (l, (instructions !! l)): visitedLines, evalInstruction l v (instructions !! l))
+
+    iterateWhileRunning'' :: (([(Int, Instruction)], a) -> Maybe ([(Int, Instruction)], a)) -> ([(Int, Instruction)], a) -> [([(Int, Instruction)], a)]
+    iterateWhileRunning'' run state = case aDash of
+                                          Just value -> value : (iterateWhileRunning'' run value)
+                                          Nothing -> []
+            where
+              aDash = run state
+
+    runInstructions'' replaceLine instructions = iterateWhileRunning'' (runLine'' instructions replaceLine) ([], (ProgrammeState 0 (Accumulator 0)))
+
+    loopMembers :: [Instruction] -> [(Int, Instruction)]
+    loopMembers instructions = fst . last $ runInstructions'' Nothing instructions
+
+    day08Pt2 = (\instructions -> filter (\(ProgrammeState n _) -> n == (length instructions)) $ map (\n -> snd . last $ runInstructions'' (Just n) instructions) $ linesToChange $ (loopMembers instructions)) <$> map (fromRight' . parse parseInstruction "") <$> readInstructions
+
+    linesToChange:: [(Int, Instruction)] -> [Int]
+    linesToChange l = map fst $ filter (canSwitchToEscape l) l
+
+    swapInstruction (Acc n) = Acc n
+    swapInstruction (Jmp n) = Nop n
+    swapInstruction (Nop n) = Jmp n
+
+    canSwitchToEscape :: [(Int, Instruction)] -> (Int, Instruction) -> Bool
+    canSwitchToEscape loopPaths (n, (Acc value)) = False
+    canSwitchToEscape loopPaths (n, (Jmp value)) = not $ (n + 1) `elem` (map fst loopPaths)
+    canSwitchToEscape loopPaths (n, (Nop value)) = not $ (n + value) `elem` (map fst loopPaths)
+
+
 
     readInstructions :: IO [String]
     readInstructions = lines <$> readFile "resource/2020/day08"
