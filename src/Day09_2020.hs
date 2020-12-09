26b91c3ccae6f9c6a39897e208fecaea98e78afc
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2020-12-09 13:49:37 +0000

    main: Day 09 2020 pt 2

--- src/Day09_2020.hs
+++ src/Day09_2020.hs
@@ -2,16 +2,22 @@ module Day09_2020
     (
     ) where
 
-    import Data.Set (Set)
-    import qualified Data.Set as Set
+    import Data.List (delete)
 
-    validate :: Int -> Set Int -> Bool
-    validate n numbers = 1 <= (Set.size $ Set.filter (\l -> Set.member (n - l) (Set.delete l numbers)) numbers)
+    validate :: Int -> [Int] -> Bool
+    validate n numbers = 1 <= (length $ filter (\l ->(n - l) `elem` (delete l numbers)) numbers)
 
-    gatherPreamble :: Int -> [Int] -> (Set Int, Int)
-    gatherPreamble n numbers = (Set.fromList $ take n numbers, head $ drop n numbers)
+    gatherPreamble :: Int -> [Int] -> ([Int], Int)
+    gatherPreamble n numbers = (take n numbers, head $ drop n numbers)
 
-    window :: Int -> [Int] -> [[Int]]
+
+    allContiguousRegions :: [a] -> [[a]]
+    allContiguousRegions list = (\n -> window n list) =<< [1..(length list)]
+
+    findRangeForInvalidNumber :: Int -> [Int] -> [Int]
+    findRangeForInvalidNumber n numbers = head $ filter (\x -> sum x == n) $ allContiguousRegions numbers
+
+    window :: Int -> [a] -> [[a]]
     window n numbers | length numbers < n = [[]]
                      | length numbers == n = [numbers]
                      | otherwise = (take n numbers) : window n (tail numbers)
@@ -20,6 +26,14 @@ module Day09_2020
     findInvalidCode n = snd . head . filter (\l -> not $ validate (snd l) (fst l)) . map (gatherPreamble n) . (window (n+1)) <$> readNumbers
 
     day09Pt1 = findInvalidCode 25
+
+    calculateWeakness :: [Int] -> Int
+    calculateWeakness l = (maximum l)+(minimum l)
+
+    searchFileForRegion n = (findRangeForInvalidNumber n) . takeWhile (/= n) <$> readNumbers
+
+    day09Pt2 = calculateWeakness <$> searchFileForRegion 1309761972
+
 
     readNumbers :: IO [Int]
     readNumbers = map read . lines <$> readFile "resource/2020/day09"
