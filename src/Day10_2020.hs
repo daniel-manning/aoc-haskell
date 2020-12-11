8baf9504a4b663d90ead1e827d791c82ee258daf
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2020-12-11 14:46:09 +0000

    main: day 10 2020 pt 2

--- src/Day10_2020.hs
+++ src/Day10_2020.hs
@@ -1,4 +1,4 @@
-module Day09_2020
+module Day10_2020
     (
     ) where
 
@@ -8,9 +8,18 @@ module Day09_2020
     gapsBetweenData [x,y] = [y-x]
     gapsBetweenData (x:y:xs) = (y-x) : gapsBetweenData (y:xs)
 
+    itemsThatCanBeDropped :: [Int] -> [Int]
+    itemsThatCanBeDropped [x,y,z] = if (z-x <= 3) then [y] else []
+    itemsThatCanBeDropped (x:y:z:xs) = if (z-x <= 3) then y:itemsThatCanBeDropped(y:z:xs) else itemsThatCanBeDropped(y:z:xs)
+
     --(\x -> (1 + fst.head x) * (1 + fst.last x)) $
     day10Pt1 =  map (\x -> (length x, nub x)) . group . sort . gapsBetweenData . sort <$> readNumbers
+    day10Pt2 =  product . map (\x -> droppablePerms (fst x)) . filter (\n -> snd n /= 3) . map (\x -> (length x, head $ nub x)) . group . gapsBetweenData . sort <$> readNumbers
 
+    droppablePerms 1 = 1
+    droppablePerms 2 = 2
+    droppablePerms 3 = 4
+    droppablePerms 4 = 7
 
     readNumbers :: IO [Int]
     readNumbers = map read . lines <$> readFile "resource/2020/day10"
