05a8a34ca667e562ab5146c930716a0cff544650
D <daniel.manning@digital.hmrc.gov.uk>
2020-12-18 20:44:11 +0000

    Day 18 2020 pt1

--- src/Day18_2020.hs
+++ src/Day18_2020.hs
@@ -4,7 +4,7 @@ module Day18_2020
     import Text.ParserCombinators.Parsec
     import Data.Either.Combinators
 
-    data Expr = Val Int | Add | Multiply deriving Show
+    data Expr = Val Int | Brackets [Expr] | Add | Multiply deriving Show
 
 
     evaluate Add (Val x) (Val y) = x + y
@@ -25,13 +25,31 @@ module Day18_2020
            n <- many1 digit
            return $ Val (read n)
 
+    parseBracket :: Parser Expr
+    parseBracket = do
+           char '('
+           n <- many1 (choice [try parseBracket, try parseVal, try parseAdd, try parseMultiply])
+           char ')'
+           return $ Brackets n
+
     parseApp :: Parser [Expr]
-    parseApp = do many1 (choice [try parseVal, try parseAdd, try parseMultiply])
+    parseApp = do many1 (choice [try parseBracket, try parseVal, try parseAdd, try parseMultiply])
 
     foldlBy2 :: [Expr] -> Expr -> Int
     foldlBy2 [x,y] z = evaluate x z y
-    foldlBy2 (x:y:xs) z = foldlBy2 xs $! (Val (evaluate x z y))
+    foldlBy2 (x:y:xs) z = foldlBy2 xs $! Val (evaluate x z y)
+
+    evaluateSimpleBrackets :: Expr -> Expr
+    evaluateSimpleBrackets (Brackets contents) | null [ x | x@(Brackets _) <- contents ] = Val (evaluateExpression contents)
+                                               | otherwise = Brackets $! map evaluateSimpleBrackets contents
+    evaluateSimpleBrackets a = a
+
+    flattenBrackets xs | null [ x | x@(Brackets _) <- xs ] = Val (evaluateExpression xs)
+                       | otherwise = flattenBrackets (map evaluateSimpleBrackets xs)             
 
     evaluateExpression (x:xs) = foldlBy2 xs x
 
-    test input = evaluateExpression $ fromRight' $ parse parseApp "" input
+    day18Pt1 = sum . map ((\(Val n) -> n) . flattenBrackets . fromRight' . parse parseApp "") <$> readSums
+
+    readSums :: IO [String]
+    readSums = lines <$> readFile "resource/2020/day18"
