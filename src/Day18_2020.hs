a10a771ff03b601323f13e750f5912d19dcbce46
D <daniel.manning@digital.hmrc.gov.uk>
2020-12-18 22:39:23 +0000

    Day 18 2020 pt2

--- src/Day18_2020.hs
+++ src/Day18_2020.hs
@@ -7,9 +7,6 @@ module Day18_2020
     data Expr = Val Int | Brackets [Expr] | Add | Multiply deriving Show
 
 
-    evaluate Add (Val x) (Val y) = x + y
-    evaluate Multiply (Val x) (Val y) = x * y
-
     parseAdd :: Parser Expr
     parseAdd = do
         string " + "
@@ -35,21 +32,41 @@ module Day18_2020
     parseApp :: Parser [Expr]
     parseApp = do many1 (choice [try parseBracket, try parseVal, try parseAdd, try parseMultiply])
 
+    ----
+    evaluate Add (Val x) (Val y) = x + y
+    evaluate Multiply (Val x) (Val y) = x * y
+
+    evaluateSimpleBrackets :: ([Expr] -> Int) -> Expr -> Expr
+    evaluateSimpleBrackets f (Brackets contents) | null [ x | x@(Brackets _) <- contents ] = Val (f contents)
+                                                 | otherwise = Brackets $! map (evaluateSimpleBrackets f) contents
+    evaluateSimpleBrackets _ a = a
+
+    flattenBrackets :: ([Expr] -> Int) -> [Expr] -> Expr
+    flattenBrackets f xs | null [ x | x@(Brackets _) <- xs ] = Val (f xs)
+                         | otherwise = flattenBrackets f (map (evaluateSimpleBrackets f) xs)             
+
+    evaluateExpression :: [Expr] -> Int
+    evaluateExpression (x:xs) = foldlBy2 xs x
+
     foldlBy2 :: [Expr] -> Expr -> Int
+    foldlBy2 [] (Val n) = n
     foldlBy2 [x,y] z = evaluate x z y
     foldlBy2 (x:y:xs) z = foldlBy2 xs $! Val (evaluate x z y)
 
-    evaluateSimpleBrackets :: Expr -> Expr
-    evaluateSimpleBrackets (Brackets contents) | null [ x | x@(Brackets _) <- contents ] = Val (evaluateExpression contents)
-                                               | otherwise = Brackets $! map evaluateSimpleBrackets contents
-    evaluateSimpleBrackets a = a
+    day18Pt1 = sum . map ((\(Val n) -> n) . flattenBrackets evaluateExpression . fromRight' . parse parseApp "") <$> readSums
+    -----------
+    --need to walk through the expression and do the sums first
+    doSumsFirst :: [Expr] -> [Expr]
+    doSumsFirst [] = []
+    doSumsFirst [x,Add,z]    = [Val (evaluate Add x z)]
+    doSumsFirst [x,y,z]      = [x,y,z]
+    doSumsFirst (x:Add:z:xs) = Val (evaluate Add x z) : xs
+    doSumsFirst (x:Multiply:z:xs) = x:Multiply: doSumsFirst (z:xs)
 
-    flattenBrackets xs | null [ x | x@(Brackets _) <- xs ] = Val (evaluateExpression xs)
-                       | otherwise = flattenBrackets (map evaluateSimpleBrackets xs)             
+    findAllSums xs | null [ x | x@Add <- xs ] = xs
+                   | otherwise = findAllSums $! doSumsFirst xs
 
-    evaluateExpression (x:xs) = foldlBy2 xs x
-
-    day18Pt1 = sum . map ((\(Val n) -> n) . flattenBrackets . fromRight' . parse parseApp "") <$> readSums
+    day18Pt2 =  sum . map ((\(Val n) -> n) . flattenBrackets (evaluateExpression . findAllSums) . fromRight' . parse parseApp "") <$> readSums
 
     readSums :: IO [String]
     readSums = lines <$> readFile "resource/2020/day18"
