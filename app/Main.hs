13c84fb812a998368ee2664ad2c54190d15749b3
D <daniel.manning@digital.hmrc.gov.uk>
2021-08-06 21:06:31 +0100

    update lts

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import Day15_2020
+import Day24_2020
 
 main :: IO ()
-main = putStrLn . show $ day15Pt2
+main = putStrLn . show =<< day24Pt2
+--main = putStrLn $ show =<< day23Pt2
