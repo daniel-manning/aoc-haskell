e7d3ab2dff5b94437e2736b95e0751565aaaf05e
Daniel Manning <djamanning@gmail.com>
2021-12-05 14:49:21 +0000

    Day 05 2021 pt2

--- src/2021/Day05_2021.hs
+++ src/2021/Day05_2021.hs
@@ -1,5 +1,6 @@
-module Day04_2021
+module Day05_2021
     (
+        solution
     ) where
 
     import Text.ParserCombinators.Parsec (many1, digit, Parser, parse, char, string)
@@ -42,12 +43,37 @@ module Day04_2021
     overlapLines s [] = s
     overlapLines s (x: xs) = overlapLines (foldl union s crosses) xs
         where
-            crosses = map (intersections x) xs
+            crosses = map (inter x) xs
 
     runPt1 :: [Line] -> Int
     runPt1 = length . overlapLines empty . filter isHorizontalOrVertical 
-    --NB: Not massively efficient we could actually just work out where they overlap if anywhere
+    
+
+    runPt2 = length . overlapLines empty 
     ---------------------------------
 
+    sgn :: Int -> Int -> Int
+    sgn 0 b = signum b
+    sgn a 0 = signum a
+    sgn a b = signum a * signum b
 
-    main =  runPt1 <$> readAndParse
+    inter :: Line -> Line -> Set Point
+    inter (L p1 p2) (L p3 p4) = fromList [point p1 t1 | t1 <- [min 0 m1 .. max 0 m1], t2 <- [min 0 m2 .. max 0 m2], v p1 p2 p3 p4 == (t2 * fst step2 - t1 * fst step1, t2 * snd step2 - t1 * snd step1)]
+        where
+            v (P x1 y1) _ (P x3 y3) _ = (x1 - x3, y1 -y3)
+            step p1@(P x1 y1) p2@(P x2 y2) = let k = mult p1 p2 in ((x2 - x1) `div` k, (y2 - y1) `div` k)
+            mult (P x1 y1) (P x2 y2) = sgn (x2 -x1) (y2 - y1) * max (abs (x2 - x1)) (abs (y2 - y1))
+            m1 = mult p1 p2
+            m2 = mult p3 p4
+            step1 = step p1 p2
+            step2 = step p3 p4
+            point (P x1 y1) n = P (x1 + n * fst step1) (y1 + n * snd step1)
+
+    solution =  runPt2 <$> readAndParse
+
+    {--
+    Compiled time to run with stack
+    real	2m3.239s
+    user	2m4.738s
+    sys	0m0.611s
+    --}
