4c6f6b7dc23906807899c9c4da7bd17a2a40b45d
Daniel Manning <djamanning@gmail.com>
2021-12-06 17:30:36 +0000

    do things the not stupid way

--- src/2021/Day06_2021.hs
+++ src/2021/Day06_2021.hs
@@ -4,7 +4,10 @@ module Day06_2021
 
     import Text.ParserCombinators.Parsec (many1, digit, Parser, parse, char, sepBy1)
     import Data.Either.Combinators
-    import Data.List(sort)
+    import Data.List(sort, find, delete, group)
+    import Data.Maybe(maybe)
+
+    data State = State Int Int deriving (Eq, Show)
 
     parseFishes :: Parser [Int]
     parseFishes = do
@@ -12,7 +15,7 @@ module Day06_2021
         return $ map read n
 
     readData :: IO String
-    readData = readFile "resource/2021/day06_test"
+    readData = readFile "resource/2021/day06"
 
     readAndParse :: IO [Int]
     readAndParse = fromRight' . parse parseFishes "" <$> readData
@@ -31,22 +34,27 @@ module Day06_2021
     afterNDays n = nTimes n nextDay
     runPt1 = length . afterNDays 80
     ------------------------
-    children n x = [m  |  m <- [0..n], (m - x - 1) `mod` 7 == 0]
-    nextGeneration n x = if n - x < 9 then [] 
-                         else [x + 9 + 7*m | m <- [0 .. (n - 9) `div` 7], x + 9 + 7*m <= n]
+    nextDayBucket :: [State] -> [State]
+    nextDayBucket [] = []
+    nextDayBucket ((State 0 n):s) = State 6 n:State 8 n:nextDayBucket s
+    nextDayBucket ((State day num): s) = State (day-1) num : nextDayBucket s
 
-    gather :: Int -> (Int -> Int -> [Int]) -> [Int] -> [Int]
-    gather n f = sort . concatMap (f n)
+    collectUpBuckets :: [State] -> [State]
+    collectUpBuckets [] = []
+    collectUpBuckets ((State d n):s) = f : collectUpBuckets s'
+        where
+            f =  maybe (State d n) (\(State _ b) -> State d (n + b)) $ find (\(State a b) -> a == d) s
+            s' = maybe s (`delete` s) $ find (\(State a b) -> a == d) s
 
-    accumulateNextGeneration :: Int -> [Int] -> [Int] -> [Int]
-    accumulateNextGeneration n old [] = old
-    accumulateNextGeneration n old new = accumulateNextGeneration n (old ++ new) (gather n nextGeneration new)
 
-    run :: Int -> [Int] -> [Int]
-    run n firstGen = accumulateNextGeneration n firstGen (gather n children firstGen)
+    groupDays :: [Int] -> [State]
+    groupDays = map (\x -> State (head x) (length x)) . group . sort
 
-    runPt1_redux = length . run 80
-    runPt2 = length . run 256 
+    countUp = foldl (\c (State d n) -> c + n) 0
+
+    runPt1_r = countUp . nTimes 80 (collectUpBuckets.nextDayBucket) . groupDays
+    runPt2 = countUp . nTimes 256 (collectUpBuckets.nextDayBucket) . groupDays
+
     -----------------------
 
     solution = runPt2 <$> readAndParse
