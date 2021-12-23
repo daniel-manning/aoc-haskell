59db90043a2cbdb2b57c16169b682e460f6c83d6
Daniel Manning <djamanning@gmail.com>
2021-12-23 22:58:04 +0000

    Day 15 2021 pt2

--- src/2021/Day15_2021.hs
+++ src/2021/Day15_2021.hs
@@ -1,13 +1,14 @@
 module Day15_2021 (solution) where
 
-import Data.Maybe (catMaybes, fromJust)
+import Data.Maybe (catMaybes, fromJust, fromMaybe)
 import Data.List (find, (\\), minimumBy)
 import Data.Function ( on ) 
 import Data.Set (Set, fromList, intersection, delete)
-import qualified Data.Set as Set (map)
+import qualified Data.Set as Set (map, filter)
 import qualified Data.Map.Strict as Map
+import qualified Data.OrdPSQ as PSQ
+import Data.Tuple.Extra (fst3)
 
-import Debug.Trace (trace)
 
 data Position  = Position Int Int deriving (Ord, Eq, Show)
 data Risk = Infinity | Risk Int deriving (Eq, Show)
@@ -49,48 +50,79 @@ startPosition = Position 0 0
 endPosition :: Foldable t => [t a] -> Position
 endPosition xs = Position (length (head xs) - 1) (length xs - 1)
 
-startingUnvisited :: Position -> Set Position
-startingUnvisited (Position maxX maxY) = fromList [Position x y | x <- [0..maxX], y <- [0..maxY]]
+startingUnvisited :: Position -> PSQ.OrdPSQ Position Risk Position
+startingUnvisited (Position maxX maxY) = foldl (\b a -> PSQ.insert a Infinity a b) PSQ.empty [Position x y | x <- [0..maxX], y <- [0..maxY]]
 
-tentativeDistances :: Position -> Map.Map Position Risk
-tentativeDistances (Position maxX maxY) = Map.update (\a -> Just (Risk 0)) startPosition $ Map.fromList [(Position x y, Infinity) | x <- [0..maxX], y <- [0..maxY]]
 
-markUpNodeDistances :: Position -> Set Position -> Map.Map Position Risk -> [[Int]] -> Map.Map Position Risk
-markUpNodeDistances current unvisited nodeRisks xs | current == finish = updatedRisks
-                                                   | otherwise = {- trace (show newCurrent)  -}markUpNodeDistances newCurrent newUnvisited updatedRisks xs
+tentativeDistances :: Position -> PSQ.OrdPSQ Position Risk Position
+tentativeDistances = PSQ.insert (Position 0 0) (Risk 0) (Position 0 0) . startingUnvisited 
+
+markUpNodeDistances :: Position -> PSQ.OrdPSQ Position Risk Position -> [[Int]] -> PSQ.OrdPSQ Position Risk Position
+markUpNodeDistances current nodeRisks xs | current == finish = updatedRisks
+                                                   | otherwise = markUpNodeDistances newCurrent newUnvisited xs
     where
         finish = endPosition xs
-        unvisitedNeighbours = nextStep finish xs current `intersection` unvisited
-        currentNodeRisk = fromJust $ Map.lookup current nodeRisks
+        unvisitedNeighbours = Set.filter (`PSQ.member` nodeRisks) $ nextStep finish xs current
+        currentNodeRisk = fst $ fromJust $ PSQ.lookup current nodeRisks
         updatedRisks = foldl (\b a -> updateRisk currentNodeRisk (get a xs) b a) nodeRisks unvisitedNeighbours
-        newUnvisited = delete current unvisited
-        newCurrent = fst $ minimumBy (compare `on` snd) $ Set.map (\p -> (p , fromJust $ Map.lookup p updatedRisks)) newUnvisited 
+        newUnvisited = PSQ.delete current updatedRisks
+        newCurrent = fst3 $ fromJust $ PSQ.findMin newUnvisited 
 
-updateRisk :: Risk -> Int -> Map.Map Position Risk -> Position -> Map.Map Position Risk
+updateRisk :: Risk -> Int -> PSQ.OrdPSQ Position Risk Position -> Position -> PSQ.OrdPSQ Position Risk Position
 updateRisk Infinity _ _ _ = error "Why is current node unset?"
-updateRisk (Risk n) edge risks position = Map.update (`smallestRisk` newRisk) position risks
+updateRisk (Risk n) edge risks position = PSQ.insert position (min oldRisk newRisk) position risks
     where
         newRisk = Risk (n + edge)
+        oldRisk = fst $ fromMaybe (newRisk, position) $ PSQ.lookup position risks
+        
 
-smallestRisk :: Risk -> Risk -> Maybe Risk
-smallestRisk a b = Just $ min a b
-
-startSearch :: [[Int]] -> Map.Map Position Risk
-startSearch xs = markUpNodeDistances startPosition (startingUnvisited finish) (tentativeDistances finish) xs
+startSearch :: [[Int]] -> PSQ.OrdPSQ Position Risk Position
+startSearch xs = markUpNodeDistances startPosition (tentativeDistances finish) xs
     where
         finish = endPosition xs
 
 minimumRisk :: [[Int]] -> Risk
-minimumRisk xs = fromJust $ Map.lookup (endPosition xs) $ startSearch xs
+minimumRisk xs = fst $ fromJust $ PSQ.lookup (endPosition xs) $ startSearch xs
 
+runPt1 :: [[Int]] -> Risk
 runPt1 = minimumRisk
 
 {--
-PT1:
+PT1 - with Map for risking:
 real	0m20.454s
 user	0m33.995s
 sys	0m16.447s
 --}
 
+{--
+PT1 - with priority search queue
+real	0m0.411s
+user	0m0.413s
+sys	0m0.072s
+--}
+
+---------------------------
+repeatBlock :: Int -> Int -> (Int -> Int -> Int) -> [[Int]] -> [[Int]]
+repeatBlock n m f xs = foldl (\b a -> b ++ map (doAndSum n a f) xs) [] [1..n]
+
+doAndSum :: Int -> Int -> (Int -> Int -> Int) -> [Int] -> [Int]
+doAndSum n m f x = foldl (\b a -> b ++ map (f (a+m)) x) (map (f m) x) [1..(n-1)]
+
+modWrap :: Int -> Int -> Int
+modWrap n m | n + m - 1 <= 9 = n + m - 1
+            | otherwise = (n + m - 1) `mod` 9
+
+createFullGrid :: [[Int]] -> [[Int]]
+createFullGrid = repeatBlock 5 5 modWrap 
+
+runPt2 :: [[Int]] -> Risk
+runPt2 = minimumRisk . createFullGrid
+
+{--
+real	0m9.386s
+user	0m11.593s
+sys	0m2.241s
+--}
+
 solution :: IO Risk
-solution = runPt1 <$> readAndParse
+solution = runPt2 <$> readAndParse
