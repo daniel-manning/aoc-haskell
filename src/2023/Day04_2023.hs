71d1c87c8433c436c291a8b777ea0a75ec0d82cd
Daniel Manning <djamanning@gmail.com>
2023-12-05 00:14:44 +0000

    Day 04 2023 pt2

--- src/2023/Day04_2023.hs
+++ src/2023/Day04_2023.hs
@@ -2,7 +2,7 @@ module Day04_2023 (
 ) where
 
 import Text.ParserCombinators.Parsec
-    ( digit, letter, string, char, anyChar, parse, try, (<|>), Parser, many, many1, sepBy1, manyTill )
+    ( digit, string, char, parse, (<|>), Parser, many, many1, sepBy1 )
 import Data.Either.Combinators
 import Data.Maybe (mapMaybe)
 import Data.List.Extra (trim)
@@ -44,3 +44,28 @@ score c | n == 0 = 0
         n = noOfWinningNumbers c
 
 runPt1 = sum . map score <$> readData
+------------------
+scatchcards :: [Card] -> [(Int, Card)]
+scatchcards = map (\c@(Card id _ _) -> (id, c))
+
+initialScratchCards :: [Card] -> [(Int, Int, Card)]
+initialScratchCards cs =  map (\(id, c) -> (id, 1, c)) $ scatchcards cs
+
+scoreFlow :: (Int, Int, Card) -> [(Int, Int, Card)] -> [(Int, Int, Card)] -> [(Int, Int, Card)]
+scoreFlow c done [] = c:done -- last card can't win anything by requirements
+scoreFlow t@(id, n, c) done cs = scoreFlow (head cs') (t:done) (tail cs')
+    where
+        s = noOfWinningNumbers c
+        ns = replicate s n
+        cs' = addToCards ns cs
+
+
+addToCards :: [Int] -> [(Int, Int, Card)] -> [(Int, Int, Card)]
+addToCards is cs = zipWith (\n (i, m, c) -> (i, n+m, c)) is (take (length is) cs) ++ drop (length is) cs
+
+playCards :: [Card] -> [(Int, Int, Card)]
+playCards cs = scoreFlow (head isc) [] (tail isc)
+    where
+        isc = initialScratchCards cs
+
+runPt2 = sum . map (\(_, n, _) -> n) . playCards <$> readData
